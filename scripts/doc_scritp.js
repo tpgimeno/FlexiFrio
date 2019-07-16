08757d5ac570731455c46e9e5e502496d85fa1e6
tonyllo <tonyllomouse@gmail.com>
2019-07-16 21:14:44 +0200

    Prueba de subida web

---
+++ scripts/doc_scritp.js
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+// JavaScript Document
+$(document).ready(function(e)
+{
+    function crear_cookies()
+	{
+		document.cookie = "";
+	}
+	
+	
+	
+	
+	
+=======
+// JavaScript Document
+$(document).ready(function(e)
+{
+    function crear_cookies()
+	{
+		document.cookie = "";
+	}
+	
+	
+	
+	
+	
+>>>>>>> d6cf558f4c59b3b58453f6147d5d9bdd7bc27dcc
+});

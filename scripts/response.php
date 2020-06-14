03fa37b3132dcdb725d7ef100371c773df25f321
tpgimeno <info@tpgimeno.com>
2020-06-14 15:40:36 +0200

    Actualización master a versión 2.0

---
+++ scripts/response.php
@@ -0,0 +1,3 @@
+<?php
+
+$response = 'El email ha sido enviado, en breve nos pondremos en contacto con usted.';
--- scripts/sendemail.php
+++ scripts/sendemail.php
@@ -5,11 +5,13 @@ $nombre = $_POST['name'];
 $email = $_POST['email'];
 $coment = $_POST['coment'];
 $headers = 'From: ' . $email;
-$mensaje = $nombre . "desea informacion sobre el alquiler/renting." . "\n" . "Sus datos de contacto son: " . "\n" . " - Email: " . $email . "\n";
+$mensaje = $nombre . "desea informacion sobre el alquiler/renting." . "\n" . "Sus datos de contacto son: " . "\n" . " - Email: " . $email . "\n" . " - Telefono: " . $tel . "\n" . " - Poblacion: " . $pob;
 $para = 'alquileres@automotiveservices.es'; 
 $asunto = 'Solicitud informacion Renting'; 
 if(mail($para, $asunto, $mensaje, $headers)){
-	header('Location: ../index.html'); 
+	$url = '../index.html';
+	$http_response_header = $this->get_headers(); 
+	var_dump($http_response_header);die;
 }else{
 echo "Error sending email";
 }
